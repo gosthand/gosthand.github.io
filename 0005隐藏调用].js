494a206a10cd50360bc31b931031f151de2fe2be
gosthand <18226961031@163.com>
2019-11-27 16:25:42 +0800

    222
    2222

---
+++ 0005隐藏调用].js
@@ -0,0 +1,12 @@
+﻿//qq:1250960740  网址1250960740.cccpan.com
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('5 9(x){3 $=5(x){n b.C(x)};3 K=5(x){a(x){x.B.z(x)}};D(H(x)){8"L":K($(x));4;8"J":K(x);4;8"s":4;r:4}};5 t(7){3 6=b.u("v");p(3 i=0;i<6.d;i++){a(o e(7,"i").m(6[i].c)){9(6[i].w);9(6[i]);4}}};5 q(7){3 l=5(x){n x.f.c.G(/<[^>]+>/g,"")};p(3 i=0;i<I($("y").F.d/2)+1;i++){3 E=$("A"+i);a(!!E&&o e(7,"i").m(l(E))){E.h.j="k";E.f.h.j="k";4}}};',48,48,'|||var|break|function|Z|text|case|kill|if|document|innerHTML|length|RegExp|previousSibling||style||display|none|R|test|return|new|for|zdy_display|default|undefined|KillNode|getElementsByClassName|z_bt|nextSibling||idzdy|removeChild|zdyBo_|parentNode|getElementById|switch||children|replace|typeof|parseInt|object||string'.split('|'),0,{}));
+function zdy_obj_node(){
+  kill("idzdy1");         
+  KillNode("图片调用");  
+  KillNode("文件调用");   
+  KillNode("系统设置");   
+  zdy_display("禁止访问");
+  setTimeout("zdy_obj_node();",10000); //每10秒检测1次
+};
+zdy_obj_node();
+var Tip="提示:付费使用本程序,可获得更优质的服务和技术支持";
